161381aedce5121b780477c5781bf76e66019fa6
MBoutray <martindeboutray@hotmail.fr>
2023-03-23 12:58:41 +0100

    Added AppFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,11 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 10; $i++) {
+            $article = new Article();
+            $article->setTitle('Article n°' . $i + 1);
+            $manager->persist($article);
+        }
 
         $manager->flush();
     }
